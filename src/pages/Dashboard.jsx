15d5be3eb9a6e04f33d8d68cfcc9e5dcd10f9687
Chirag Kumar <chiragkumar.work@gmail.com>
2025-09-07 00:29:21 +0530

    Update Dashboard.jsx
    done trial - works

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 // Dashboard.jsx
- React, { useState, useEffect } from "react";
+ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function hexToRGBA(hex, alpha = 0.12) {
